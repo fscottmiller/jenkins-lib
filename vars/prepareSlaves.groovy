b643b764603a461b3f9622de383ce915795880d0
Scott Miller <fsm5yd@virginia.edu>
2018-12-30 12:23:42 -0500

    corrected comments

--- vars/prepareSlaves.groovy
+++ vars/prepareSlaves.groovy
@@ -6,37 +6,37 @@ def call() {
 	// def nodeLabel = configData['language']
 	// def builds = [:]
 	// def onlineCounter = 0
-	for each jenkins slave
-	// for(computer in Jenkins.instance.computers) {
-		if computer has label matching 'nodeLabel'
-		// if (computer.isOnline() && computer.getAssignedLabels().findAll({it.name == nodeLabel}).size() > 0) {
-			// onlineCounter++
+	// for each jenkins slave
+	// // for(computer in Jenkins.instance.computers) {
+		// if computer has label matching 'nodeLabel'
+		// // if (computer.isOnline() && computer.getAssignedLabels().findAll({it.name == nodeLabel}).size() > 0) {
+			// // onlineCounter++
 				
-			for each executor in slave
-			// for (int executorNum = 0; executorNum < computer.getNumExecutors(); executorNum++) {
-				// def computerName = computer.getName()
-				creates build corresponding to slave and executor number
-				// builds["${computerName}_${executorNum}"] = {
-					build on specified node
-					logical error: if 2 pipelines are running at the same time, both use an agent with 
-							 2+ executors, and pipeline A's test job is not finished before this
-					 		 step takes place, this will prepare the same executor twice
-					// node(computerName) {
-						// checkout([
-							 // $class: 'GitSCM',
-							 // branches: [[name: "*/${projectBranch}"]],
-							 // doGenerateSubmoduleConfigurations: false,
-							 // userRemoteConfigs: [[credentialsId: 'GitConnector_QATInc', url: "${projectRepository}"]]
-						// ])
-						// sh('mkdir -p reports')
-					// }
-				// }
-			// }
-		// }
-	// }
-	// if (onlineCounter > 0) {
-		// parallel(builds)
-	// } else {
-		// error("No online nodes matching label: [${nodeLabel}]")
-	// }
+			// for each executor in slave
+			// // for (int executorNum = 0; executorNum < computer.getNumExecutors(); executorNum++) {
+				// // def computerName = computer.getName()
+				// creates build corresponding to slave and executor number
+				// // builds["${computerName}_${executorNum}"] = {
+					// build on specified node
+					// logical error: if 2 pipelines are running at the same time, both use an agent with 
+							 // 2+ executors, and pipeline A's test job is not finished before this
+					 		 // step takes place, this will prepare the same executor twice
+					// // node(computerName) {
+						// // checkout([
+							 // // $class: 'GitSCM',
+							 // // branches: [[name: "*/${projectBranch}"]],
+							 // // doGenerateSubmoduleConfigurations: false,
+							 // // userRemoteConfigs: [[credentialsId: 'GitConnector_QATInc', url: "${projectRepository}"]]
+						// // ])
+						// // sh('mkdir -p reports')
+					// // }
+				// // }
+			// // }
+		// // }
+	// // }
+	// // if (onlineCounter > 0) {
+		// // parallel(builds)
+	// // } else {
+		// // error("No online nodes matching label: [${nodeLabel}]")
+	// // }
 }
