8d84680913902c222e08f66c99385f354ac7ade9
Scott Miller <fsm5yd@virginia.edu>
2018-12-30 18:07:13 -0500

    work on gitClone

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String branch, String credentials, String repository) {
+def call(String branch, String credentials="", String repository) {
     checkout([
         $class: 'GitSCM',
         branches: [[name: "*/${branch}"]],
