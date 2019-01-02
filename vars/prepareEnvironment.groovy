c874765e6d355e2800addce44e006242cc447f96
Scott Miller <fsm5yd@virginia.edu>
2019-01-02 09:03:21 -0500

    update prepateEnvironment to take language as param

--- vars/prepareEnvironment.groovy
+++ vars/prepareEnvironment.groovy
@@ -1,23 +1,19 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(language) {
     echo "Preparing environment..."
-	//checkout repo
-
-    //
-	// switch(projectLanguage) {
-		// case 'ruby':
-			// runner = load('lib_groovy/ruby.groovy')
-			// break
-		// case 'junit':
-			// runner = load('lib_groovy/junit.groovy')
-			// break
-		// case 'specflow':
-			// runner = load('lib_groovy/specflow.groovy')
-			// break
-		// default:
-			// error('Project language from jenkins-config.yml is not correct/supported')
-			// break
-	// }
-	// runner.prepareSlaves(projectName, projectRepository, projectBranch, projectEnvironment)
+	switch(projectLanguage) {
+		case 'ruby':
+			echo 'Provisioning ruby env...'
+			break
+		case 'junit':
+			echo 'Provisioning java env...'
+			break
+		case 'specflow':
+			echo 'Provisioning c# env...'
+			break
+		default:
+			error('Project language from config.yml is not yet supported')
+			break
+	}
 }
