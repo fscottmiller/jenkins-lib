d8165660044d9714d480af26cc56a8fa626c66a3
Scott Miller <fsm5yd@virginia.edu>
2019-01-02 11:43:58 -0500

    test

--- vars/var0/gitClone.groovy
+++ vars/var0/gitClone.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String repository, String branch, String dir=".") {
+def call(String repository, String branch) {
     echo "${branch}"
     echo "${repository}"
     checkout([
         $class: 'GitSCM',
         branches: [[name: "*/${branch}"]],
         doGenerateSubmoduleConfigurations: false,
-        userRemoteConfigs: [[url: "${repository}"]],
-        extensions: [[$class: 'RelativeTargetDirectory', relateiveTargetDir: dir]]
+        userRemoteConfigs: [[url: "${repository}"]]
+        //extensions: [[$class: 'RelativeTargetDirectory', relateiveTargetDir: dir]]
     ])
 }
