c0296f5b7c364b9e360f9b66e58ed62dfa9f0673
nickblantz <nicholasblantz@gmail.com>
2019-01-03 14:42:31 -0500

    Debugging new config stuff

--- src/com/qaas/pipeline/JenkinsConfig.groovy
+++ src/com/qaas/pipeline/JenkinsConfig.groovy
@@ -1,12 +1,27 @@
 package com.qaas.pipeline
 
 class JenkinsConfig implements Serializable {
-	public JenkinsConfig config
-	
+	public static JenkinsConfig config
 	protected Map dataMap = [:]
 	
 	public JenkinsConfig(Map dataMap) {
 		this.dataMap = dataMap
 		config = this
+	}
+	
+	public static String getLanguage() {
+		return config['language']
+	}
+	
+	public static String getEnvironmentType(String environment) {
+		return config['environments'][environment]
+	}
+	
+	public static String[] getEnvironmentStages(Sring environment) {
+		return config['environments'][environment]
+	}
+	
+	public static String[] getEmailList() {
+		return config['email']
 	}
 }
--- vars/readConfig.groovy
+++ vars/readConfig.groovy
@@ -1,6 +1,5 @@
-//import com.qaas.pipeline
+import com.qaas.pipeline.JenkinsConfig
 
 def call() {
 	new com.qaas.pipeline.JenkinsConfig(readYaml(file: "config.yml"))
-	println("Hello, World!")
 }
